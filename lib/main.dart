b025fb2d6c601c8f7704d36aef8cc1bf7bbb506b
OsamahAbdullah99 <osamababaeer@gmail.com>
2022-02-01 04:05:21 +0800

    updated yaml

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test',
+      title: 'PA Advisor',
       debugShowCheckedModeBanner: false,
       home: LoginWidget(),
     );
