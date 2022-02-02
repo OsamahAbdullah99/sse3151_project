e88b8f255aa151d455fea935e514791ecf604ec8
OsamahAbdullah99 <osamababaeer@gmail.com>
2022-02-02 15:42:17 +0800

    New UI login and register

--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'register.dart';
+import 'background.dart';
 
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 
 
@@ -16,260 +18,152 @@ class LoginWidget extends StatefulWidget {
 class _LoginWidgetState extends State<LoginWidget> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-              'PA Advisor'
-          ),
-          centerTitle: true,
-          backgroundColor: Color.fromRGBO(0, 0, 0, 0.25),
-          elevation: 0.0,
-
-        ),
-      body: Container(
-        // width: 375,
-        // height: 812,
-        decoration: BoxDecoration(
-          color : Colors.blue
-          //fromRGBO(108, 99, 255, 1)
-          //fromRGBO(255, 255, 255, 1),
-        ),
-        child: Stack(
-            children: <Widget>[
-              // Positioned(
-              //     top: 0,
-              //     left: 1,
-              //     child: SvgPicture.asset(
-              //         'assets/images/backgroundbox.png',
-              //         semanticsLabel: 'backgroundbox'
-              //     )
-              // ),
-              // Positioned(
-              //     top: 48,
-              //     left: 142,
-              //     child: Text('PAdvisor', textAlign: TextAlign.left, style: TextStyle(
-              //         color: Color.fromRGBO(255, 255, 255, 1),
-              //         fontFamily: 'Poppins',
-              //         fontSize: 24,
-              //         letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-              //         fontWeight: FontWeight.normal,
-              //         height: 1
-              //     ),
-              //     )
-              // ),
-              Positioned(
-                  top: 266,
-                  left: 44,
-                  child: Container(
-                      width: 287,
-                      height: 440,
-                      decoration: BoxDecoration(
-                        borderRadius : BorderRadius.only(
-                          topLeft: Radius.circular(50),
-                          topRight: Radius.circular(50),
-                          bottomLeft: Radius.circular(50),
-                          bottomRight: Radius.circular(50),
-                        ),
-                        boxShadow : const [BoxShadow(
-                            color: Color.fromRGBO(0, 0, 0, 0.25),
-                            offset: Offset(0,2),
-                            blurRadius: 3
-                        )],
-                        color : Color.fromRGBO(255, 246, 246, 1),
-                      )
-                  )
-              ),Positioned(
-                  top: 405,
-                  left: 74,
-                  child: Container(
-                      width: 232,
-                      height: 35,
-                      decoration: BoxDecoration(
-                        borderRadius : BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                          bottomLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                        ),
-                        color : Color.fromRGBO(108, 99, 255, 1),
-                      )
-                  )
-              ),Positioned(
-                  top: 409,
-                  left: 152,
-                  child: Text('Password', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 1),
-                      fontFamily: 'Poppins',
-                      fontSize: 18,
-                      letterSpacing: 0,
-                      fontWeight: FontWeight.normal,
-                      height: 1
-                  ),)
-              ),Positioned(
-                  top: 348,
-                  left: 74,
-                  child: Container(
-                      width: 232,
-                      height: 35,
-                      decoration: BoxDecoration(
-                        borderRadius : BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                          bottomLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                        ),
-                        color : Color.fromRGBO(108, 99, 255, 1),
-                      )
-                  )
-              ),Positioned(
-                  top: 352,
-                  left: 145,
-                  child: Text('Username', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 1),
-                      fontFamily: 'Poppins',
-                      fontSize: 18,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1
-                  ),)
-              ),Positioned(
-                  top: 481,
-                  left: 74,
-                  child: Container(
-                      width: 232,
-                      height: 35,
-                      decoration: BoxDecoration(
-                        borderRadius : BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                          bottomLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                        ),
-                        color : Color.fromRGBO(108, 99, 255, 1),
-                      )
-                  )
-              ),Positioned(
-                  top: 487,
-                  left: 154,
-                  child: Text('Sign in', textAlign: TextAlign.left, style: TextStyle(
-                      color: Colors.deepOrange,
-                      //fromRGBO(255, 255, 255, 1),
-                      fontFamily: 'Poppins',
-                      fontSize: 24,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.bold,
-                      height: 1
-                  ),)
-              ),Positioned(
-                  top: 447,
-                  left: 129,
-                  child: Text('Forgot password? ', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(57, 98, 241, 1),
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1
-                  ),)
-              ),Positioned(
-                  top: 728,
-                  left: 65,
-                  child: Text('Don’t have an account? Sign Up!', textAlign: TextAlign.left, style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Roboto',
-                      fontSize: 18,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1
-                  ),)
-              ),Positioned(
-                  top: 573,
-                  left: 95,
-                  child: Divider(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      thickness: 1
-                  )
-
-              ),Positioned(
-                  top: 545,
-                  left: 164,
-                  child: Container(
-                      width: 49,
-                      height: 51,
-                      decoration: BoxDecoration(
-                        color : Color.fromRGBO(229, 229, 229, 1),
-                        borderRadius : BorderRadius.all(Radius.elliptical(49, 51)),
-                      )
-                  )
-              ),Positioned(
-                  top: 555,
-                  left: 173,
-                  child: Text('OR', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontFamily: 'Poppins',
-                      fontSize: 24,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1
-                  ),)
-              ),Positioned(
-                  top: 619,
-                  left: 81,
-                  child: Container(
-                      width: 209,
-                      height: 31,
-                      decoration: BoxDecoration(
-                        borderRadius : BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                          bottomLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                        ),
-                        color : Color.fromRGBO(255, 255, 255, 1),
-                      )
-                  )
-              ),Positioned(
-                  top: 618,
-                  left: 95,
-                  child: Container(
-                      width: 30,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        image : DecorationImage(
-                            image: AssetImage('assets/images/Google1.png'),
-                            fit: BoxFit.fitWidth
-                        ),
-                      )
-                  )
-              ),Positioned(
-                  top: 623,
-                  left: 138,
-                  child: Text('Login with Google', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(108, 99, 255, 1),
-                      fontFamily: 'Poppins',
-                      fontSize: 15,
-                      letterSpacing: 0,
-                      fontWeight: FontWeight.normal,
-                      height: 1
-                  ),)
-              ),Positioned(
-                  top: 100,
-                  left: 55,
-                  child: Container(
-                      width: 264,
-                      height: 135,
-                      decoration: BoxDecoration(
-                        image : DecorationImage(
-                            image: AssetImage('assets/images/1.png'),
-                            fit: BoxFit.fitWidth
-                        ),
-                      )
-                  )
+      body: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2661FA),
+                    fontSize: 36
+                ),
+                textAlign: TextAlign.left,
               ),
-            ]
-        )
-    )
+            ),
+
+            SizedBox(height: size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Email"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Password"
+                ),
+                obscureText: true,
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: Text(
+                "Forgot your password?",
+                style: TextStyle(
+                    fontSize: 12,
+                    color: Color(0XFF2661FA)
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.05),
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: new BoxDecoration(
+                      borderRadius: BorderRadius.circular(80.0),
+                      gradient: new LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 255, 136, 34),
+                            Color.fromARGB(255, 255, 177, 41)
+                          ]
+                      )
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  child: Text(
+                    "LOGIN",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: new BoxDecoration(
+                      borderRadius: BorderRadius.circular(80.0),
+                      gradient: new LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 255, 136, 34),
+                            Color.fromARGB(255, 255, 177, 41)
+                          ]
+                      )
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  child: Text(
+                    "Login with Google",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: GestureDetector(
+                onTap: () => {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()))
+                },
+                child: Text(
+                  "Don't Have an Account? Sign up",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2661FA)
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PA Advisor',
+      title: 'Test',
       debugShowCheckedModeBanner: false,
       home: LoginWidget(),
     );
