3a54c814bf8254778c0aa4819b1c65f23bd21163
204309 <204309@student.upm.edu.my>
2022-02-03 02:03:06 +0800

    Update: add Form

--- lib/login.dart
+++ lib/login.dart
@@ -6,8 +6,6 @@ import 'background.dart';
 
 //import 'package:google_fonts/google_fonts.dart';
 
-
-
 class LoginWidget extends StatefulWidget {
   const LoginWidget({Key? key}) : super(key: key);
 
@@ -20,10 +18,8 @@ class _LoginWidgetState extends State<LoginWidget> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
-
       body: Background(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -36,56 +32,48 @@ class _LoginWidgetState extends State<LoginWidget> {
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
                     color: Color(0xFF2661FA),
-                    fontSize: 36
-                ),
+                    fontSize: 36),
                 textAlign: TextAlign.left,
               ),
             ),
-
             SizedBox(height: size.height * 0.03),
-
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: "Email"
+            Form(
+                child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextField(
+                    decoration: InputDecoration(labelText: "Email"),
+                  ),
                 ),
-              ),
-            ),
-
-            SizedBox(height: size.height * 0.03),
-
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: "Password"
+                SizedBox(height: size.height * 0.03),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextField(
+                    decoration: InputDecoration(labelText: "Password"),
+                    obscureText: true,
+                  ),
                 ),
-                obscureText: true,
-              ),
-            ),
-
+              ],
+            )),
             Container(
               alignment: Alignment.centerRight,
               margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
               child: Text(
                 "Forgot your password?",
-                style: TextStyle(
-                    fontSize: 12,
-                    color: Color(0XFF2661FA)
-                ),
+                style: TextStyle(fontSize: 12, color: Color(0XFF2661FA)),
               ),
             ),
-
             SizedBox(height: size.height * 0.05),
             Container(
               alignment: Alignment.centerRight,
               margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
               child: RaisedButton(
                 onPressed: () {},
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
                 textColor: Colors.white,
                 padding: const EdgeInsets.all(0),
                 child: Container(
@@ -94,31 +82,26 @@ class _LoginWidgetState extends State<LoginWidget> {
                   width: size.width * 0.5,
                   decoration: new BoxDecoration(
                       borderRadius: BorderRadius.circular(80.0),
-                      gradient: new LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 255, 136, 34),
-                            Color.fromARGB(255, 255, 177, 41)
-                          ]
-                      )
-                  ),
+                      gradient: new LinearGradient(colors: [
+                        Color.fromARGB(255, 255, 136, 34),
+                        Color.fromARGB(255, 255, 177, 41)
+                      ])),
                   padding: const EdgeInsets.all(0),
                   child: Text(
                     "LOGIN",
                     textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
-                    ),
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
             ),
-
             Container(
               alignment: Alignment.centerRight,
               margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
               child: RaisedButton(
                 onPressed: () {},
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)),
                 textColor: Colors.white,
                 padding: const EdgeInsets.all(0),
                 child: Container(
@@ -127,39 +110,33 @@ class _LoginWidgetState extends State<LoginWidget> {
                   width: size.width * 0.5,
                   decoration: new BoxDecoration(
                       borderRadius: BorderRadius.circular(80.0),
-                      gradient: new LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 255, 136, 34),
-                            Color.fromARGB(255, 255, 177, 41)
-                          ]
-                      )
-                  ),
+                      gradient: new LinearGradient(colors: [
+                        Color.fromARGB(255, 255, 136, 34),
+                        Color.fromARGB(255, 255, 177, 41)
+                      ])),
                   padding: const EdgeInsets.all(0),
                   child: Text(
                     "Login with Google",
                     textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
-                    ),
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
             ),
-
             Container(
               alignment: Alignment.centerRight,
               margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
               child: GestureDetector(
                 onTap: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()))
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RegisterScreen()))
                 },
                 child: Text(
                   "Don't Have an Account? Sign up",
                   style: TextStyle(
                       fontSize: 12,
                       fontWeight: FontWeight.bold,
-                      color: Color(0xFF2661FA)
-                  ),
+                      color: Color(0xFF2661FA)),
                 ),
               ),
             )
