2c18ceae20d897f2cb92eeb0ae2fa23b9b7caf8e
204309 <204309@student.upm.edu.my>
2022-02-03 15:43:35 +0800

    Update Google SignIN

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'login.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
