954c0fc155922c434c3eaf6e5c708924410b6822
204309 <204309@student.upm.edu.my>
2022-02-10 22:51:52 +0800

    pdate: Editting Student Profile (in Progress)

--- lib/student/editProfile.dart
+++ lib/student/editProfile.dart
@@ -131,156 +131,159 @@ class editProfile extends StatelessWidget {
               end: Alignment.bottomCenter),
         ),
         alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 40,
-              backgroundImage: NetworkImage(image ??
-                  'https://firebasestorage.googleapis.com/v0/b/padvisor-45b73.appspot.com/o/default2_stdicon.jpg?alt=media&token=2e4518de-036f-47b6-9010-23588e9a6fe4'),
-            ),
-            SizedBox(height: 8),
-            TextFormField(
-              // initialValue: currentName,
-              controller: FNCtrl,
-              decoration: InputDecoration(
-                labelText: 'Full Name',
-                hintText: currentName,
-                fillColor: Colors.white,
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                ),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundImage: NetworkImage(image ??
+                    'https://firebasestorage.googleapis.com/v0/b/padvisor-45b73.appspot.com/o/default2_stdicon.jpg?alt=media&token=2e4518de-036f-47b6-9010-23588e9a6fe4'),
               ),
-              validator: (val) => val!.isEmpty ? 'Please enter a name' : null,
-              // onChanged: (val) => setState(() {
-              //   currentName = val;
-              // }),
-            ),
-            SizedBox(height: 8),
-            TextFormField(
-              // initialValue: currentUPMID,
-              controller: MIDCtrl,
-              decoration: InputDecoration(
-                labelText: 'UPM-ID',
-                hintText: currentUPMID,
-                fillColor: Colors.white,
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
+              SizedBox(height: 8),
+              TextFormField(
+                // initialValue: currentName,
+                controller: FNCtrl,
+                decoration: InputDecoration(
+                  labelText: 'Full Name',
+                  hintText: currentName,
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
                 ),
+                validator: (val) => val!.isEmpty ? 'Please enter a name' : null,
+                // onChanged: (val) => setState(() {
+                //   currentName = val;
+                // }),
               ),
-              validator: (val) => val != null && val.length < 6
-                  ? 'This field cannot be empty'
-                  : null,
-              // onChanged: (val) => setState(() {
-              //   currentUPMID = val;
-              // }),
-            ),
-            SizedBox(height: 8),
-            // TextFormField(
-            //   initialValue: currentName,
-            //   decoration: InputDecoration(
-            //     labelText: 'Full Name',
-            //     hintText: currentName,
-            //     fillColor: Colors.white,
-            //     filled: true,
-            //     enabledBorder: OutlineInputBorder(
-            //       borderSide: BorderSide(color: Colors.white, width: 2.0),
-            //     ),
-            //   ),
-            //   validator: (val) => val!.isEmpty ? 'Please enter a name' : null,
-            //   onChanged: (val) => setState(() {
-            //     currentName = val;
-            //   }),
-            // ),
+              SizedBox(height: 8),
+              TextFormField(
+                // initialValue: currentUPMID,
+                controller: MIDCtrl,
+                decoration: InputDecoration(
+                  labelText: 'UPM-ID',
+                  hintText: currentUPMID,
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                ),
+                validator: (val) => val != null && val.length < 6
+                    ? 'This field cannot be empty'
+                    : null,
+                // onChanged: (val) => setState(() {
+                //   currentUPMID = val;
+                // }),
+              ),
+              SizedBox(height: 8),
+              // TextFormField(
+              //   initialValue: currentName,
+              //   decoration: InputDecoration(
+              //     labelText: 'Full Name',
+              //     hintText: currentName,
+              //     fillColor: Colors.white,
+              //     filled: true,
+              //     enabledBorder: OutlineInputBorder(
+              //       borderSide: BorderSide(color: Colors.white, width: 2.0),
+              //     ),
+              //   ),
+              //   validator: (val) => val!.isEmpty ? 'Please enter a name' : null,
+              //   onChanged: (val) => setState(() {
+              //     currentName = val;
+              //   }),
+              // ),
 
-            // SizedBox(height: 8),
-            // Text(
-            //   'Email: ' + user.email!,
-            //   style: TextStyle(fontSize: 16),
-            // ),
-            // //phone number is not available for those who sign in through google.
-            // //so, we need the profile that can be edited to update their phone number
+              // SizedBox(height: 8),
+              // Text(
+              //   'Email: ' + user.email!,
+              //   style: TextStyle(fontSize: 16),
+              // ),
+              // //phone number is not available for those who sign in through google.
+              // //so, we need the profile that can be edited to update their phone number
 
-            // SizedBox(height: 8),
-            // Padding(
-            //   padding: const EdgeInsets.symmetric(
-            //       horizontal: 30, vertical: 10),
-            //   child: Container(
-            //     decoration: BoxDecoration(
-            //       color: Colors.white70,
-            //       borderRadius: new BorderRadius.circular(10.0),
-            //     ),
-            //     // child: Padding(
-            //     //   padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            //     child: TextFormField(
-            //       initialValue: user.displayName!,
-            //       style: TextStyle(fontSize: 16),
-            //       decoration: InputDecoration(
-            //         border: OutlineInputBorder(),
-            //         fillColor: Colors.white,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // ),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: ElevatedButton(
-                onPressed: () async {
-                  if (formKey.currentState!.validate()) {
-                    name = FNCtrl.text.trim();
-                    UPMID = MIDCtrl.text.trim();
-                    // final String semester = semCtrl.text;
-                    // final String faculty = _faculty!;
-                    // final String email = emailCtrl.text.trim();
-                    // final String phoneNumber = PNCtrl.text.trim();
-                    // final String wechat = wcCtrl.text.trim();
-                    // final String password = passwordCtrl.text.trim();
+              // SizedBox(height: 8),
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(
+              //       horizontal: 30, vertical: 10),
+              //   child: Container(
+              //     decoration: BoxDecoration(
+              //       color: Colors.white70,
+              //       borderRadius: new BorderRadius.circular(10.0),
+              //     ),
+              //     // child: Padding(
+              //     //   padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              //     child: TextFormField(
+              //       initialValue: user.displayName!,
+              //       style: TextStyle(fontSize: 16),
+              //       decoration: InputDecoration(
+              //         border: OutlineInputBorder(),
+              //         fillColor: Colors.white,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              // ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      name = FNCtrl.text.trim();
+                      UPMID = MIDCtrl.text.trim();
+                      // final String semester = semCtrl.text;
+                      // final String faculty = _faculty!;
+                      // final String email = emailCtrl.text.trim();
+                      // final String phoneNumber = PNCtrl.text.trim();
+                      // final String wechat = wcCtrl.text.trim();
+                      // final String password = passwordCtrl.text.trim();
 
-                    User user = FirebaseAuth.instance.currentUser!;
+                      User user = FirebaseAuth.instance.currentUser!;
 
-                    await FirebaseFirestore.instance
-                        .collection("users")
-                        .doc(user.uid)
-                        .update({
-                      'fullName': name,
-                      'upmid': UPMID,
-                      // 'email': email,
-                      // 'phoneNumber': '',
-                    });
+                      await FirebaseFirestore.instance
+                          .collection("students")
+                          .doc(user.uid)
+                          .update({
+                        'fullName': name,
+                        'upmid': UPMID,
+                        // 'email': email,
+                        // 'phoneNumber': '',
+                      });
 
-                    Navigator.pop(context);
-                  }
-                },
-                style: ButtonStyle(
-                    padding: MaterialStateProperty.all(EdgeInsets.all(0)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80.0),
-                    ))),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50.0,
-                  width: size.width * 0.5,
-                  decoration: new BoxDecoration(
-                      borderRadius: BorderRadius.circular(80.0),
-                      gradient: new LinearGradient(colors: [
-                        Color.fromARGB(255, 255, 136, 34),
-                        Color.fromARGB(255, 255, 177, 41)
-                      ])),
-                  padding: const EdgeInsets.all(0),
-                  child: Text(
-                    "Save",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                      Navigator.pop(context);
+                    }
+                  },
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.all(EdgeInsets.all(0)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0),
+                      ))),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 50.0,
+                    width: size.width * 0.5,
+                    decoration: new BoxDecoration(
+                        borderRadius: BorderRadius.circular(80.0),
+                        gradient: new LinearGradient(colors: [
+                          Color.fromARGB(255, 255, 136, 34),
+                          Color.fromARGB(255, 255, 177, 41)
+                        ])),
+                    padding: const EdgeInsets.all(0),
+                    child: Text(
+                      "Save",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
