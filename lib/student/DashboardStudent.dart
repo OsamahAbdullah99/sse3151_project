36f6e506145f396f1e87ecb2cc40ecc249068b20
OsamahAbdullah99 <osamababaeer@gmail.com>
2022-02-19 03:23:21 +0800

    Update: updating student dashboard file

--- lib/student/DashboardStudent.dart
+++ lib/student/DashboardStudent.dart
@@ -2,6 +2,7 @@ import 'package:SSE3151_project/background2.dart';
 import 'package:SSE3151_project/services/menu_item.dart';
 import 'package:SSE3151_project/services/menu_lists.dart';
 import 'package:SSE3151_project/student/profile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'loginPage.dart';
@@ -15,156 +16,173 @@ class dashboardStudent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
+    // final user = FirebaseAuth.instance.currentUser;
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        automaticallyImplyLeading: false,
-        title: Text(
-          "Dashboard",
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        actions: [
-          PopupMenuButton<MenuItem>(
-            onSelected: (item) => onSelected(context, item),
-            itemBuilder: (context) => [
-              ...MenuLists.itemLists.map(buildItem).toList(),
-              PopupMenuDivider(),
-              ...MenuLists.itemSecList.map(buildItem).toList(),
-            ],
-            color: Colors.white,
-            // icon: new Icon(Icons.add, color: Colors.amber),
-          ),
-          // TextButton(
-          //     onPressed: () {
-          //       // final provider =
-          //       //     Provider.of<GoogleSignInProvider>(context, listen: false);
-          //       // provider.logout();
-          //       FirebaseAuth.instance.signOut();
-          //       Navigator.push(
-          //           context,
-          //           MaterialPageRoute(
-          //               builder: (context) => LoginWidget(
-          //                   // onClickedSignUp: () {},
-          //                   )));
-          //     },
-          //     child: Image.asset(
-          //       'assets/images/logoutIcon.png',
-          //       scale: 20,
-          //     )),
-        ],
-      ),
-      body: Background2(
-        child: Card(
-          margin: EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  alignment: Alignment.centerLeft,
-                  padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: Text(
-                    "",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF2661FA),
-                        fontSize: 27),
-                    textAlign: TextAlign.center,
-                  ),
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('Announcement')
+            .doc('Posts')
+            .collection('Announcement Lists')
+            .where('postTo', isEqualTo: 'Student')
+            .snapshots(),
+        initialData: null,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.indigo,
+                automaticallyImplyLeading: false,
+                title: Text(
+                  "Dashboard",
+                  style: TextStyle(color: Colors.white),
                 ),
-                SizedBox(height: 10),
-                Card(
-                  elevation: 10,
-                  child: Container(
-                    // height: 400,
-                    width: 300,
-                    padding: EdgeInsets.all(20),
-                    margin: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.white60,
-                    ),
-                    child: Column(
-                      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 10,
-                            horizontal: 5,
-                          ),
-                          color: Colors.red[20],
-                          child: Text(
-                            'Assignment1',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 30),
-                        Container(
-                          child: Text(
-                            '$text',
-                            style: TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                centerTitle: true,
+                actions: [
+                  PopupMenuButton<MenuItem>(
+                    onSelected: (item) => onSelected(context, item),
+                    itemBuilder: (context) => [
+                      ...MenuLists.itemLists.map(buildItem).toList(),
+                      PopupMenuDivider(),
+                      ...MenuLists.itemSecList.map(buildItem).toList(),
+                    ],
+                    color: Colors.white,
+                    // icon: new Icon(Icons.add, color: Colors.amber),
                   ),
-                ),
-                Card(
-                  elevation: 10,
-                  child: Container(
-                    width: 300,
-                    padding: EdgeInsets.all(20),
-                    margin: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Colors.white60,
-                    ),
-                    child: Column(
-                      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 10,
-                            horizontal: 5,
-                          ),
-                          color: Colors.red[20],
-                          child: Text(
-                            'Assignment1',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 30),
-                        Container(
-                          child: Text(
-                            '$text',
-                            style: TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+                  // TextButton(
+                  //     onPressed: () {
+                  //       // final provider =
+                  //       //     Provider.of<GoogleSignInProvider>(context, listen: false);
+                  //       // provider.logout();
+                  //       FirebaseAuth.instance.signOut();
+                  //       Navigator.push(
+                  //           context,
+                  //           MaterialPageRoute(
+                  //               builder: (context) => LoginWidget(
+                  //                   // onClickedSignUp: () {},
+                  //                   )));
+                  //     },
+                  //     child: Image.asset(
+                  //       'assets/images/logoutIcon.png',
+                  //       scale: 20,
+                  //     )),
+                ],
+              ),
+              body: Background2(
+                child: postList(context, snapshot),
+                // Card(
+                //   margin: EdgeInsets.all(20),
+                //   child: SingleChildScrollView(
+                //     child: Column(
+                //       mainAxisAlignment: MainAxisAlignment.center,
+                //       children: <Widget>[
+                //         Container(
+                //           alignment: Alignment.centerLeft,
+                //           padding: EdgeInsets.symmetric(horizontal: 30),
+                //           child: Text(
+                //             "",
+                //             style: TextStyle(
+                //                 fontWeight: FontWeight.bold,
+                //                 color: Color(0xFF2661FA),
+                //                 fontSize: 27),
+                //             textAlign: TextAlign.center,
+                //           ),
+                //         ),
+                //         SizedBox(height: 10),
+                //         Card(
+                //           elevation: 10,
+                //           child: Container(
+                //             // height: 400,
+                //             width: 300,
+                //             padding: EdgeInsets.all(20),
+                //             margin: EdgeInsets.all(5),
+                //             decoration: BoxDecoration(
+                //               color: Colors.white60,
+                //             ),
+                //             child: Column(
+                //               // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //               crossAxisAlignment: CrossAxisAlignment.start,
+                //               children: [
+                //                 Container(
+                //                   padding: EdgeInsets.symmetric(
+                //                     vertical: 10,
+                //                     horizontal: 5,
+                //                   ),
+                //                   color: Colors.red[20],
+                //                   child: Text(
+                //                     'Assignment1',
+                //                     style: TextStyle(
+                //                       fontWeight: FontWeight.bold,
+                //                       fontStyle: FontStyle.italic,
+                //                       fontSize: 20,
+                //                     ),
+                //                   ),
+                //                 ),
+                //                 SizedBox(height: 30),
+                //                 Container(
+                //                   child: Text(
+                //                     '$text',
+                //                     style: TextStyle(
+                //                       fontSize: 16,
+                //                     ),
+                //                   ),
+                //                 ),
+                //               ],
+                //             ),
+                //           ),
+                //         ),
+                //         Card(
+                //           elevation: 10,
+                //           child: Container(
+                //             width: 300,
+                //             padding: EdgeInsets.all(20),
+                //             margin: EdgeInsets.all(5),
+                //             decoration: BoxDecoration(
+                //               color: Colors.white60,
+                //             ),
+                //             child: Column(
+                //               // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //               crossAxisAlignment: CrossAxisAlignment.start,
+                //               children: [
+                //                 Container(
+                //                   padding: EdgeInsets.symmetric(
+                //                     vertical: 10,
+                //                     horizontal: 5,
+                //                   ),
+                //                   color: Colors.red[20],
+                //                   child: Text(
+                //                     'Assignment1',
+                //                     style: TextStyle(
+                //                       fontWeight: FontWeight.bold,
+                //                       fontStyle: FontStyle.italic,
+                //                       fontSize: 20,
+                //                     ),
+                //                   ),
+                //                 ),
+                //                 SizedBox(height: 30),
+                //                 Container(
+                //                   child: Text(
+                //                     '$text',
+                //                     style: TextStyle(
+                //                       fontSize: 16,
+                //                     ),
+                //                   ),
+                //                 ),
+                //               ],
+                //             ),
+                //           ),
+                //         ),
+                //       ],
+                //     ),
+                //   ),
+                // ),
+              ),
+            );
+          }
+        });
   }
 
   PopupMenuItem<MenuItem> buildItem(MenuItem item) {
@@ -197,10 +215,97 @@ class dashboardStudent extends StatelessWidget {
         FirebaseAuth.instance.signOut();
         Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (context) => LoginWidget()),
-          (route) => false,
+              (route) => false,
         );
         break;
       default:
     }
   }
+}
+
+// Future getPostList() async {
+//   await FirebaseFirestore.instance
+//       .collection('Announcement')
+//       .doc('Posts')
+//       .collection('Announcement Lists')
+//       .orderBy("index", descending: true)
+//       .get()
+//       .then((snapshot) => snapshot.docs.forEach((value) {
+//             // postDetails(value["postTile"], value["postContent"]);
+//             print(value['postTo']);
+//             if (value['postTo'] == "All" &&
+//                 value['postTo'] == "Students Only") {
+//               print('Entered All');
+//               postDetails(value["postTile"], value["postContent"]);
+//             }
+//             //  else if (value['postTo'] == "Lecturers Only") {
+//             //   print('Entered Lecturers');
+//             //   postDetails(value["postTile"], value["postContent"]);
+//             // }
+//           }));
+// }
+
+Widget postList(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+  return Card(
+    margin: EdgeInsets.all(20),
+    child: Container(
+      width: 350,
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.white60,
+      ),
+      child: postDetails(context, snapshot),
+    ),
+  );
+}
+
+Widget postDetails(
+    BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+  final postList = snapshot.data!.docs.toList();
+  return ListView.separated(
+      separatorBuilder: (context, index) => SizedBox(height: 4),
+      itemCount: postList.length,
+      itemBuilder: ((context, index) {
+        return Card(
+          elevation: 10,
+          child: Container(
+            width: 300,
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.white60,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 5,
+                  ),
+                  child: Text(
+                    postList[index]['postTitle'],
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  child: Text(
+                    postList[index]['postContent'],
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        );
+      }));
 }
